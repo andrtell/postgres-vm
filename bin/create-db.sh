2e76f3036837bf472ae381b02e14fb6197c85687
Andreas Tell <116459057+andrtell@users.noreply.github.com>
2023-12-13 14:29:45 +0100

    removed scripts

--- bin/create-db.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ $# -lt 1 ]]; then
-    echo "usage: create-db.sh {name}"
-    exit 1
-fi
-
-podman -r run -it --rm --network db --secret postgres_connection,type=env,target=CONN docker.io/postgres \
-    bash -c 'psql $CONN -q -c "CREATE DATABASE '"$1"';"'
